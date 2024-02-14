97c29beecf09c242e463bb65c00dd8b87c58e6b1
Jihad Sayed <david.pales1@gmail.com>
2024-02-14 13:55:43 +0100

    ds

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nee-techs'),
+      dir: require('path').join(__dirname, './coverage/neetechs-account'),
       subdir: '.',
       reporters: [
         { type: 'html' },
