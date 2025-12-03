d68f2a9ca72d3ef1166babbe8490cb133f700929
Jihad Sayed <david.pales1@gmail.com>
2025-12-03 09:48:41 -0600

    d

--- src/app/authentication/get-credential/get-credential.component.ts
+++ src/app/authentication/get-credential/get-credential.component.ts
@@ -1,18 +1,20 @@
-import { isPlatformBrowser } from "@angular/common";
-import { Component, Inject, OnInit, PLATFORM_ID } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-get-credential",
-  templateUrl: "./get-credential.component.html",
-  styleUrls: ["./get-credential.component.scss"],
+  selector: 'app-get-credential',
+  templateUrl: './get-credential.component.html',
+  styleUrls: ['./get-credential.component.scss'],
 })
 export class GetCredentialComponent implements OnInit {
-  host!: string;
-  language!: string;
-  pathname!: string;
+  host: string | null = null;
+  language: string | null = null;
+  pathname: string | null = null;
+
   userToken: string | null = null;
   userInfo: string | null = null;
+
   mainDomain = '';
   frontEndUrl = '';
   serverUrl = '';
@@ -27,8 +29,9 @@ export class GetCredentialComponent implements OnInit {
     if (isPlatformBrowser(this.platformId)) {
       let hostname = window.location.hostname;
       let origin = window.location.origin;
-      let lang = window.navigator.language?.substring(0, 2) || 'en';
+      const lang = window.navigator.language?.substring(0, 2) || 'en';
 
+      // When testing from localhost, force real domain
       if (hostname.includes('localhost')) {
         hostname = 'neetechs.com';
         origin = 'https://neetechs.com';
@@ -44,40 +47,47 @@ export class GetCredentialComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (!isPlatformBrowser(this.platformId)) return;
 
-    this.userToken = localStorage.getItem("userToken");
-    this.userInfo = localStorage.getItem("UserInfo");
+    // pull from localStorage on the accounts domain
+    this.userToken = localStorage.getItem('userToken');
+    this.userInfo = localStorage.getItem('UserInfo');
 
     this.route.queryParams.subscribe((params) => {
-      this.host = params["host"];
-      this.language = params["language"];
-      this.pathname = params["pathname"];
+      this.host = params['host'] || null;
+      this.language = params['language'] || this.language || 'en';
+      this.pathname = params['pathname'] || '';
 
-      console.log("GET CREDENTIAL PARAMS:", {
+      console.log('GET CREDENTIAL PARAMS:', {
         host: this.host,
         language: this.language,
         pathname: this.pathname,
       });
 
-      if (this.userToken && this.host) {
-        document.cookie = `userToken=${this.userToken}; domain=.neetechs.com; path=/; Secure; SameSite=None`;
-        document.cookie = `UserInfo=${this.userInfo}; domain=.neetechs.com; path=/; Secure; SameSite=None`;
+      // If no host, we can’t safely postMessage – just bail
+      if (!this.host) {
+        return;
+      }
 
+      if (this.userToken) {
+        // Sync cookies for *.neetechs.com so Django sees the session
+        document.cookie = `userToken=${this.userToken}; domain=.neetechs.com; path=/; Secure; SameSite=None`;
+        document.cookie = `UserInfo=${this.userInfo ?? ''}; domain=.neetechs.com; path=/; Secure; SameSite=None`;
+
+        // Send credential back to parent window
         window.top?.postMessage(
           {
-            type: "credential",
+            type: 'credential',
             getToken: this.userToken,
             getUserInfo: this.userInfo,
           },
           this.host
         );
-      } else if (this.host) {
+      } else {
+        // No token stored on accounts → tell parent to show login
         window.top?.postMessage(
-          {
-            type: "login_required",
-          },
+          { type: 'login_required' },
           this.host
         );
       }
